8674d1cfcd737e58b8b1d4bdb9d9b20fd669300a
FlauschigesAlex <alexindahoodmc@gmail.com>
2023-12-17 16:10:35 +0100

    v1.0.1: Library can now set the GatewayIntents for the discord bot.

--- src/main/java/at/flauschigesalex/discordLibrary/FlauschigeDiscordLibrary.java
+++ src/main/java/at/flauschigesalex/discordLibrary/FlauschigeDiscordLibrary.java
@@ -9,12 +9,30 @@ import net.dv8tion.DiscordListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 public final class FlauschigeDiscordLibrary extends FlauschigeLibrary {
 
     private static FlauschigeDiscordLibrary flauschigeDiscordLibrary;
     @Getter private static JDA JDA;
+    private static final ArrayList<GatewayIntent> gatewayIntents = new ArrayList<>();
+
+    /**
+     * Make sure to run this method in your main class!
+     * This is extremely important for reflections!
+     *
+     * @return an instance of the Library
+     */
+    public static FlauschigeDiscordLibrary getLibrary(final @NotNull GatewayIntent gatewayIntent, final @NotNull GatewayIntent... moreGatewayIntents) {
+        gatewayIntents.clear();
+        gatewayIntents.add(gatewayIntent);
+        gatewayIntents.addAll(List.of(moreGatewayIntents));
+
+        return getLibrary();
+    }
 
     /**
      * Make sure to run this method in your main class!
@@ -36,7 +54,7 @@ public final class FlauschigeDiscordLibrary extends FlauschigeLibrary {
         final JsonManager jsonManager = resourceManager.getJsonManager();
         if (jsonManager == null) return;
 
-        JDA = JDABuilder.create(jsonManager.asString("bot.token"), List.of(GatewayIntent.values())).build();
+        JDA = JDABuilder.create(jsonManager.asString("bot.token"), gatewayIntents).build();
 
         DiscordListener.registerAll();
         DiscordCommand.registerAll();
